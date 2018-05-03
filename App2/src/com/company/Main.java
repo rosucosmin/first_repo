ea51b072ea2ea1fb9e6cd3b0ae7c0842bd102319
CTBAV\Cosmin.Rosu <cosmin.rosu@ctbav.ro>
2018-05-04 01:30:07 +0300

    The main program for testing a number;

--- App2/src/com/company/Main.java
+++ App2/src/com/company/Main.java
@@ -2,15 +2,17 @@ package com.company;
 
 import java.util.*;
 
+import static java.lang.Math.pow;
+
 public class Main {
 
-    public static int f(int a)
+    public static int f( int a)
     {
         int b = a;
         int counter, sum = 0;
         LinkedList<Integer> var = new LinkedList<Integer>();
 
-        for(int div = 2; div < (a/2); div++ )
+        for(int div = 2; div < (a/2 + 1); div++ )
         {
 
             counter = 0;
@@ -29,23 +31,71 @@ public class Main {
             }
         }
 
-
         Iterator<Integer> listItr = var.iterator();
-        do
+//        do
+//        {
+//
+//            sum += listItr.next();
+//        }
+//        while(listItr.hasNext());
+
+        while(listItr.hasNext())
         {
-
-            sum = sum + listItr.next();
+            sum += listItr.next();
         }
-        while(listItr.hasNext());
 
-        return sum;
+
+            return sum;
+
+
     }
 
     public static void main(String[] args) {
 
-        int a = 90;
+        Scanner sc = new Scanner(System.in);
+        int a;
 
-        System.out.println("functia returneaza: " + f(a));
-        //Daca functia e corecta returneaza 4 pt 90;
+        do {
+            System.out.println("Introduceti un numar de doua cifre: ");
+            a = sc.nextInt();
+        }
+        while(!(a > 1 && a < 100));
+
+        int b = a, numCount = 0;
+        while(b > 0)
+        {
+
+            numCount++;
+            b = b / 10;
+        }
+
+        System.out.println(f(a));
+        System.out.println(numCount);
+
+        b=a;
+        int i = 1, count = 0;
+        while(i < numCount + 1)
+        {
+            int c = 0;
+            i++;
+            if( f(b) < 1)
+            {
+                count++;
+            }
+            c = b % 10;
+            b /=10;
+            c = (int) (c* pow(10,numCount - 1));
+            b = b + c;
+        }
+
+        if(count == numCount)
+        {
+            System.out.println("DA");
+        }
+        else
+        {
+            System.out.println("NU");
+        }
+
     }
 }
